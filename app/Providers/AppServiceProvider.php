0f66457bf05e1fd651893bddfbd9097962dae731
phich82 <phich82@gmail.com>
2019-06-19 02:27:44 +0700

    Update code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Contracts\PostRepositoryContract;
+use App\Repositories\PostRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(PostRepositoryContract::class, PostRepository::class);
     }
 }
